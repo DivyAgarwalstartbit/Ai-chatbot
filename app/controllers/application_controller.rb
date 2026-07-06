a6220ba3bb2f16d9ddcb9a1e988eb59d4c1547e4
Divy Agarwal <divyagarwal@startbitsolutions.com>
2026-07-06 11:33:56 +0530

    memory metrics added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :log_memory_before
+after_action :log_memory_after
+
+def log_memory_before
+  rss_kb = `ps -o rss= -p #{Process.pid}`.to_i
+  Rails.logger.info "[MEMORY] BEFORE #{rss_kb / 1024} MB"
+end
+
+def log_memory_after
+  rss_kb = `ps -o rss= -p #{Process.pid}`.to_i
+  Rails.logger.info "[MEMORY] AFTER #{rss_kb / 1024} MB"
+end
+
+
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
