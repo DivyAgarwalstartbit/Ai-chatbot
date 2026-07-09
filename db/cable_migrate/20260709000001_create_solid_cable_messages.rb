2b748dc484c44948e5d8e650f739afe27cc196a6
Divy Agarwal <divyagarwal@startbitsolutions.com>
2026-07-09 12:10:57 +0530

    this is the action bacle issue resolved

--- db/cable_migrate/20260709000001_create_solid_cable_messages.rb
+++ db/cable_migrate/20260709000001_create_solid_cable_messages.rb
@@ -1,14 +1,14 @@
 class CreateSolidCableMessages < ActiveRecord::Migration[8.1]
   def change
-    create_table :solid_cable_messages, force: false do |t|
+    create_table :solid_cable_messages, force: false, if_not_exists: true do |t|
       t.binary  :channel,      null: false
       t.bigint  :channel_hash, null: false
       t.binary  :payload,      null: false
       t.datetime :created_at,  null: false
     end
 
-    add_index :solid_cable_messages, :channel
-    add_index :solid_cable_messages, :channel_hash
-    add_index :solid_cable_messages, :created_at
+    add_index :solid_cable_messages, :channel,      if_not_exists: true
+    add_index :solid_cable_messages, :channel_hash, if_not_exists: true
+    add_index :solid_cable_messages, :created_at,   if_not_exists: true
   end
 end
